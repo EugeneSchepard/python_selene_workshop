2baa3270ad33c0977481fb3b48af9603d0c74c17
Sergey Pirogov <sergio_89@ukr.net>
2017-05-28 18:58:56 +0300

    .travis transfer sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-sudo pip install pipenv
-pipenv --three
-pipenv install pytest
-pipenv install pytest-env
-pipenv run pip install git+https://github.com/yashaka/selene.git
-pipenv run pip install git+https://github.com/SergeyPirogov/testcontainers-python.git
-pipenv run pip install git+https://github.com/allure-framework/allure-python2.git@master#subdirectory=allure-python-commons
-pipenv run pip install git+https://github.com/allure-framework/allure-python2.git@master#subdirectory=allure-pytest
-pipenv shell
