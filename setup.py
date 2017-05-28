c16ba78ea24dce772ec8c268428bf126441cfa00
Sergey Pirogov <sergio_89@ukr.net>
2017-05-28 16:48:11 +0300

    .travis integration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='testing_stage',
+    version='1.0',
+    packages=['src', 'src.pages', 'src.domain'],
+    url='http',
+    license='',
+    author='sergey',
+    author_email='',
+    description=''
+)
