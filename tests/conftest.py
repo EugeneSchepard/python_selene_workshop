f4f3a1414171d08f16fb333c6a3ee091a8d73bd3
Sergey Pirogov <sergio_89@ukr.net>
2017-05-28 17:04:29 +0300

    .travis integration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+import os
+
 import pytest
 from testcontainers.compose import DockerCompose
 
+
 @pytest.fixture(scope="session")
 def setup(request):
-    DockerCompose("/home/sergey/PycharmProjects/testing_stage/").start()
+    DockerCompose(os.path.join(os.path.dirname(__file__), "..")).start()
