670cd7ae26cb5a99e53cddbef06fa86b2a569c1f
Sergey Pirogov <sergio_89@ukr.net>
2017-05-28 19:52:49 +0300

    compose wait for added

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,6 @@ from testcontainers.compose import DockerCompose
 
 @pytest.fixture(scope="session")
 def setup(request):
-    DockerCompose(os.path.join(os.path.dirname(__file__), "..")).start()
+    compose = DockerCompose(os.path.join(os.path.dirname(__file__), ".."))
+    compose.start()
+    compose.wait_for("http://localhost:8086")
